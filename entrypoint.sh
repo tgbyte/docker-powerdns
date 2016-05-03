70c7e6d3771e44074c10da11679a4e8bfdd98ca0
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-05-04 00:17:18 +0200

    --no-config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 exec /usr/sbin/pdns_server \
     --launch=gmysql --gmysql-host=db --gmysql-user=$DB_USER --gmysql-dbname=$DB_NAME --gmysql-password=$DB_PASSWORD \
     --webserver=yes --webserver-address=0.0.0.0 --webserver-port=80 \
-    --experimental-json-interface=yes --experimental-api-key=$POWERDNS_API_KEY
+    --experimental-json-interface=yes --experimental-api-key=$POWERDNS_API_KEY \
+    --no-config
