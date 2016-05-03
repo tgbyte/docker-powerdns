8aeedab05500439999cbc1154e4d6bf678ab0911
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-05-03 23:03:57 +0200

    s/database/db/

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 exec /usr/sbin/pdns_server \
-    --launch=gmysql --gmysql-host=database --gmysql-user=$DB_USER --gmysql-dbname=$DB_NAME --gmysql-password=$DB_PASSWORD \
+    --launch=gmysql --gmysql-host=db --gmysql-user=$DB_USER --gmysql-dbname=$DB_NAME --gmysql-password=$DB_PASSWORD \
     --webserver=yes --webserver-address=0.0.0.0 --webserver-port=80 \
     --experimental-json-interface=yes --experimental-api-key=$POWERDNS_API_KEY
