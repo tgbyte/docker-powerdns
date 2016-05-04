0bd51450e6c3384bb2b0c52b39b97a2f8e79beb0
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-05-04 21:41:56 +0200

    ALLOW_AXFR_IPS support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ exec /usr/sbin/pdns_server \
     --launch=gmysql --gmysql-host=db --gmysql-user=$DB_USER --gmysql-dbname=$DB_NAME --gmysql-password=$DB_PASSWORD \
     --webserver=yes --webserver-address=0.0.0.0 --webserver-port=80 \
     --experimental-json-interface=yes --experimental-api-key=$POWERDNS_API_KEY \
-    --setuid=pdns --setgid=pdns --no-config
+    --setuid=pdns --setgid=pdns --no-config --allow-axfr-ips="${ALLOW_AXFR_IPS}"
