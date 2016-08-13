2f87a32de3a45011cf63a6da5baf32ac4941ff2d
Thilo-Alexander Ginkel <tg@tgbyte.de>
2016-08-14 01:09:13 +0200

    Connect to mysql instead of db

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 exec /usr/sbin/pdns_server \
-    --launch=gmysql --gmysql-host=db --gmysql-user=$DB_USER --gmysql-dbname=$DB_NAME --gmysql-password=$DB_PASSWORD \
+    --launch=gmysql --gmysql-host=mysql --gmysql-user=$DB_USER --gmysql-dbname=$DB_NAME --gmysql-password=$DB_PASSWORD \
     --webserver=yes --webserver-address=0.0.0.0 --webserver-port=80 \
     --experimental-json-interface=yes --experimental-api-key=$POWERDNS_API_KEY \
     --setuid=pdns --setgid=pdns --no-config --allow-axfr-ips="${ALLOW_AXFR_IPS}" --master
